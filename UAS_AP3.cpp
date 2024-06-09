0db5577d8cd81991a0ac3f49605ca138c9a9374e
SILVERGOLDZ <167726244+SILVERGOLDZ@users.noreply.github.com>
2024-06-09 17:17:23 +0700

    Update

--- UAS_AP3.cpp
+++ UAS_AP3.cpp
@@ -1,8 +1,8 @@
+#include <windows.h>
 #include <iostream>
 #include <conio.h>
 #include <thread>
 #include <chrono>
-#include <windows.h>
 #include <cstdlib>
 #include <conio.h>
 #include "animasi.h"
@@ -14,89 +14,152 @@ void clrl() {
     cout << "\033[2K"; //menghapus satu baris
 }
 
-void char_delay(const string& teks, int ms, int x) {
-    if (x == 1){
-        system("cls");
+void animasikan(string text, int n, int j)
+{
+    
+    while (true) {
+        for (int i = n ; i <= j; ++i) {
+            system("cls");
+            cout << monsterFrames[i] << "\n" << text << endl;
+            delay(700);
+        }
     }
 
-    for (char c : teks) {
-        cout << c << flush; 
-        Beep(650,ms);
-        delay(ms); 
-    } 
-    
+ 
 }
+
 
 void intro(){
     const char* array_string[] = {"\n\n\n\n\n\n\n\n\n\n\n                                               Tips!",
                                      "\n\n                       Tekan enter untuk melanjutkan setiap dialog yang muncul",
                                      "\n\n\n\n\n\n\n\n\n\n\n                                         ",
-                                     "selamat bermain!", "Belajar", "C++", "doumo"};
+                                     "selamat bermain!"};
 
     int arraySize = sizeof(array_string) / sizeof(array_string[0]);
     
-    char_delay(array_string[0], 10,0);
+    char_delay(array_string[0], 10,0,0);
     delay(1000);
-    char_delay(array_string[1],10,0);
-    getchar();
+    char_delay(array_string[1],10,0,1);
     system("cls");
-    char_delay(array_string[2],10,0);
-    char_delay(array_string[3],100,0);
-    getchar();
+    char_delay(array_string[2],10,0,0);
+    char_delay(array_string[3],100,0,1);
     system("cls");
 }
 
-void teks(){
-    const char* array_string[] = {"Tips!\nTekan enter untuk melanjutkan setiap dialog yang muncul",
-                                     "Selamat bermain", "Belajar", "C++", "doumo"};
-
-    int arraySize = sizeof(array_string) / sizeof(array_string[0]);
-}
 
 int main(){
     system("cls");
-    char option;
-    intro();
+    pemilihan opsi;
+    opsi.option;
+    //intro();
 
     player karakter;
     string prev_nama = "";
     while(true){
     introducer1();
-    char_delay("Bagaimana saya memanggil anda?\n",10,0);
+    char_delay("Bagaimana saya memanggil anda?\n",10,0,0);
     cin >> karakter.nama;
     delay(500);
     
     if (karakter.nama == prev_nama){
         system("cls");
         introducer3();
-        char_delay("\n...",10,0);getch();clrl();
-        char_delay("\nKamu bercanda?",10,0);getch();clrl();
-        char_delay("\nBukankah nama ini sama dengan yang sebelumnya???",10,0);getch();clrl();
-        char_delay("\n...",10,1);getch();
-        char_delay("\n...",10,1);getch();
-        char_delay("\n...",10,1);getch();
-        char_delay("\nBaiklah...",20,1);getch();
+        char_delay("\n...",10,0,1);clrl();
+        char_delay("\nKamu bercanda?",10,0,1);clrl();
+        char_delay("\nBukankah nama ini sama dengan yang sebelumnya???",10,0,1);clrl();
+        char_delay("\n...",10,1,1);
+        char_delay("\n...",10,1,1);
+        char_delay("\n...",10,1,1);
+        char_delay("\nBaiklah...",20,1,1);
     }
     prev_nama = karakter.nama;
 
     system("cls");
     introducer2();
-    char_delay("Hi, ", 10,0);delay(1000);
-    char_delay(karakter.nama + "...",200,0);
+    char_delay("Hi, ", 10,0,0);delay(1000);
+    char_delay(karakter.nama + "...",200,0,0);
     delay(1000);
-    balik:
-    char_delay("\nApakah nama ini sudah benar?(y/n)",10,0);
-    option = getch();
-        if (option == 'y'){
+    balik1:
+    char_delay("\nApakah nama ini sudah benar?(y/n)",10,0,0);
+    opsi.option = getch();
+        if (opsi.option == 'y'){
             break;
         }
-        else if(option == 'n'){
+        else if(opsi.option == 'n'){
             system("cls");
             continue;
         }
         else{
             cout << "\nerror! Pilihan hanya y atau n!\n";
-            goto balik;
+            goto balik1;
         }
     }
+
+    char_delay("Senang bertemu denganmu...",10,1,1);
+    balik2:
+    char_delay("Apakah Kamu ingin membaca prologue terlebih dahulu? (y/n)",10,1,0);
+    opsi.option = getch();
+    
+    if (opsi.option == 'y'){
+        system("cls");
+        //prologue()
+    }
+
+    else if (opsi.option =='n'){
+        system("cls");
+    }
+
+    else{
+        goto balik2;
+    }
+
+    ///////////////////////////////////////////////////////
+    boss monster;
+    player chara;
+
+    balik3:
+    char_delay("Pick your class...",10,1,0);
+    cout << klass[0];
+    opsi.option = getch();
+    if (opsi.option == '1'){
+        karakter.senjata = "sword";
+        karakter.x = 2000;
+        karakter.y = 8000;
+        karakter.hp = 1000;
+    }
+    else if(opsi.option == '2'){
+        karakter.senjata = "bow";
+        karakter.x = 1000;
+        karakter.y = 3000;
+        karakter.hp = 700;
+    }
+    else if (opsi.option == '3'){
+        karakter.senjata = "shield";
+        karakter.x = 500;
+        karakter.y = 2000;
+        karakter.hp = 500;
+    }
+    else{
+        char_delay("\nError! Pilihan hanya 1 sampai 3... (press enter to continue)",10,1,1);
+        getch();
+        goto balik3;
+    }
+    for (int i = 0 ; i < 4; ++i) {
+            system("cls");
+            cout << tulis_besar[i];
+            delay(1000);
+        }
+    
+    system ("cls");
+    delay(2000);
+
+    ////////////////////////// game on /////////////////////////////
+    cout << monsterFrames[0];
+        char_delay("Manusia bodoh...", 10, 0, 1);
+        system("cls");
+    cout << monsterFrames[0];
+        char_delay("Biar aku perlihatkan rasanya keputusasaan...!", 10, 0, 1);
+        system("cls");
+    animasikan("", 0, 6);
+
 }
--- class.h
+++ class.h
@@ -1,7 +1,88 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+
+class pemilihan{
+public:
+    char option;
+};
+
+int crit(int x, int y){
+    int multiplier = (rand() % (y - x + x)) + x;
+    return multiplier;
+}
+
+int serang(int a, int b){
+        srand(time(NULL));        
+        int damage = (rand() % (b - a + 1)) + a;
+
+        return damage;
+}
+
+class bar{
+public:
+    int hp_ui = 100;
+};
+
+class boss{
+public:
+    string nama = "Apex Dominator";
+    int hp = 100000;
+
+    int x = 70, y = 120;
+    int attack(){
+        int damage = serang(x,y);
+        int penentu_crit = crit(1,10);
+
+            if (penentu_crit == 1 || penentu_crit == 2){
+                damage *= 2;
+            }
+        return damage;
+    }
+    
+};
 
 class player{
 public:
     string nama;
+    string senjata;
+    int hp;
+
+    int x,y;
+    int attack(){
+        int damage = serang(x,y);
+        if (senjata == "sword"){
+            int penentu_crit = crit(1,5);
+
+            if (penentu_crit == 1 || penentu_crit == 2){
+                damage *= 2;
+            }
+        }
+        return damage;
+    }
+
+float scale(float z){
+    boss mons;
+
+    float hp_sebelum = mons.hp; 
+    float ahh = mons.hp - z; 
+    double hitung = 1 - (ahh / hp_sebelum);
+
+    float darah = 100 *hitung; 
+
+    return hitung;
+}
+    
+void hp_interface(int x){
+    int n = 0;
+
+    while (n < 2){
+        for (int i = 0; i <= x; ++i){
+            cout << "|";
+        }
+        cout << endl;
+        ++n;
+    }
+}
+
 };
